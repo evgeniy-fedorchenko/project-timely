dccac7aab81ba5468e16532abb8479343bb0c54d
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-09-23 21:57:24 +0500

    first-structure: configure side menu

--- app/src/main/java/com/efedorchenko/timely/MainActivity.kt
+++ app/src/main/java/com/efedorchenko/timely/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.efedorchenko.timely
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationView
 import com.jakewharton.threetenabp.AndroidThreeTen
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager2
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         setupInsets()
         setupViewPager()
+        setupSideMenu()
     }
 
     private fun setupInsets() {
@@ -36,5 +47,52 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = CalendarPageAdapter(this)
         viewPager.setCurrentItem(CalendarPageAdapter.CALENDAR_SCROLL_BORDERS / 2, false)
         viewPager.offscreenPageLimit = 3
+    }
+
+
+    private fun setupSideMenu() {
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val menuButton: ImageButton = findViewById(R.id.menu_button)
+        menuButton.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        val headerView = navigationView.getHeaderView(0)
+
+        val usernameTextView = headerView.findViewById<TextView>(R.id.user_name)
+        usernameTextView.text = "Федорченко Евгений Викторович"
+
+        val positionTextView = headerView.findViewById<TextView>(R.id.position)
+        val spannablePositionText = SpannableString("Должность: колотильщик паллетов")
+        spannablePositionText.setSpan(
+            StyleSpan(Typeface.BOLD),
+            0,
+            9,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        positionTextView.text = spannablePositionText
+
+        val rateTextView = headerView.findViewById<TextView>(R.id.rate)
+        val spannableRateText = SpannableString("Ставка: 200 руб./ч.")
+        spannableRateText.setSpan(
+            StyleSpan(Typeface.BOLD),
+            0,
+            6,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        rateTextView.text = spannableRateText
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.fill_period -> {
+                }
+
+                R.id.my_team -> {
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
     }
 }
