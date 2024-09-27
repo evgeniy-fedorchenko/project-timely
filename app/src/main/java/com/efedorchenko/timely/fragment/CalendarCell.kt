6a999ea1b31c27134456cedd879541f22a2d66d6
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-09-27 19:12:50 +0500

    first-structure: add Builder and Helper for calendar's cells build

---
+++ app/src/main/java/com/efedorchenko/timely/fragment/CalendarCell.kt
@@ -0,0 +1,80 @@
+package com.efedorchenko.timely.fragment
+
+import android.view.View.OnClickListener
+import com.efedorchenko.timely.R
+import com.efedorchenko.timely.event.Event
+import org.threeten.bp.DayOfWeek.SATURDAY
+import org.threeten.bp.DayOfWeek.SUNDAY
+import org.threeten.bp.LocalDate
+
+data class CalendarCell(
+    val text: String,
+    val textStyle: Int,
+    val parentBackground: Int,
+    val onClickListener: OnClickListener?,
+    val event: Event?,
+) {
+
+    class Builder {
+
+        private var date: LocalDate = LocalDate.now()
+        private var type: CellType = CellType.CURRENT_MONTH
+        private var onClickListener: OnClickListener? = null
+        private var event: Event? = null
+
+        fun setDate(date: LocalDate) = apply {
+            this.date = date
+        }
+
+        fun setType(type: CellType) = apply {
+            this.type = type
+        }
+
+        fun setOnClickListener(listener: OnClickListener) = apply {
+            this.onClickListener = listener
+        }
+
+        fun setEvent(event: Event?) = apply {
+            this.event = event
+        }
+
+        fun build(): CalendarCell {
+            val text = date.dayOfMonth.toString()
+            val textStyle: Int
+            val parentBackground: Int
+
+            when (type) {
+                CellType.PAST_MONTH -> {
+                    textStyle = R.style.inactive_date
+                    parentBackground = R.drawable.inactive_day
+                }
+
+                CellType.CURRENT_MONTH -> {
+                    textStyle = R.style.active_date
+
+                    val isWeekend = date.dayOfWeek == SATURDAY || date.dayOfWeek == SUNDAY
+                    val isToday = LocalDate.now().isEqual(date)
+                    parentBackground = when {
+                        isWeekend && isToday -> R.drawable.weekend_current
+                        isWeekend && !isToday -> R.drawable.weekend_ordinary
+                        !isWeekend && isToday -> R.drawable.weekday_current
+                        else -> R.drawable.weekday_ordinary
+                    }
+                }
+
+                CellType.NEXT_MONTH -> {
+                    textStyle = R.style.inactive_date
+                    parentBackground = R.drawable.inactive_day
+                }
+            }
+            return CalendarCell(text, textStyle, parentBackground, onClickListener, event)
+        }
+    }
+
+    enum class CellType {
+        PAST_MONTH,
+        CURRENT_MONTH,
+        NEXT_MONTH
+    }
+
+}
---
+++ app/src/main/java/com/efedorchenko/timely/fragment/CalendarHelper.kt
@@ -0,0 +1,43 @@
+package com.efedorchenko.timely.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import com.efedorchenko.timely.fragment.MonthFragment.Companion.SELECTED_DATE_KEY
+import org.threeten.bp.LocalDate
+
+class CalendarHelper(private val context: Context) {
+
+    fun oldDateSelected(): Toast {
+        val toast = Toast.makeText(
+            context,
+            "Эта дата уже прошла",
+            Toast.LENGTH_LONG
+        )
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        return toast
+    }
+
+    fun rejectRewriteEvent(): Toast {
+        val toast = Toast.makeText(
+            context,
+            "Запланированную смену нельзя редактировать!",
+            Toast.LENGTH_LONG
+        )
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        return toast
+    }
+
+    fun eventDialog(date: LocalDate, fragment: MonthFragment): AddEventDialog {
+        val bundle = Bundle()
+        val selectedDateStr = date.toString()
+        bundle.putString(SELECTED_DATE_KEY, selectedDateStr)
+
+        val addEventDialog = AddEventDialog.newInstance(fragment)
+        addEventDialog.arguments = bundle
+        return addEventDialog
+
+    }
+
+}
