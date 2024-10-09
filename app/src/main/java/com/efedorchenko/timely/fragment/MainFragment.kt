08d04ba5f7c000a74528f6814675f3e099166584
evgeniy-fedorchenko <jecky432@gmail.com>
2024-10-10 00:29:36 +0500

    first-structure: few fixed

--- app/src/main/java/com/efedorchenko/timely/fragment/MainFragment.kt
+++ app/src/main/java/com/efedorchenko/timely/fragment/MainFragment.kt
@@ -1,6 +1,5 @@
 package com.efedorchenko.timely.fragment
 
-import android.app.Application
 import android.graphics.Typeface
 import android.os.Bundle
 import android.text.Spannable
@@ -18,12 +17,9 @@ import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.efedorchenko.timely.R
-import com.efedorchenko.timely.model.Fine
-import com.efedorchenko.timely.repository.FineRepository
 import com.efedorchenko.timely.service.CalendarPageAdapter
 import com.efedorchenko.timely.service.MainViewModel
 import com.google.android.material.navigation.NavigationView
-import org.threeten.bp.LocalDate
 
 class MainFragment : Fragment() {
 
@@ -49,46 +45,8 @@ class MainFragment : Fragment() {
         calendarFragment = CalendarFragment()
 
         setupViewPager(view)
-        setupSummaryCard()
+        setupSummaryCard(view)
         setupSideMenu(view)
-        initFines()
-    }
-
-    private fun initFines() {
-        val fineRepository = FineRepository(requireActivity().applicationContext as Application)
-
-        fineRepository.save(
-            Fine(LocalDate.parse("2024-08-01"), "some desc", 100),
-            Fine(LocalDate.parse("2024-08-02"), "some desc", 200),
-            Fine(LocalDate.parse("2024-08-03"), "some desc", 300),
-            Fine(LocalDate.parse("2024-08-04"), "some desc", 400),
-            Fine(LocalDate.parse("2024-08-05"), "some desc", 500),
-            Fine(LocalDate.parse("2024-08-06"), "some desc", 600),
-            Fine(LocalDate.parse("2024-09-07"), "some desc", 700),
-            Fine(LocalDate.parse("2024-09-08"), "some desc", 800),
-            Fine(LocalDate.parse("2024-09-09"), "some desc", 900),
-            Fine(LocalDate.parse("2024-09-10"), "some desc", 1000),
-            Fine(LocalDate.parse("2024-09-11"), "some desc", 1100),
-            Fine(LocalDate.parse("2024-09-12"), "some desc", 1200),
-            Fine(LocalDate.parse("2024-10-13"), "some desc", 1300),
-            Fine(LocalDate.parse("2024-10-14"), "some desc", 1400),
-            Fine(LocalDate.parse("2024-10-15"), "some desc", 1500),
-            Fine(LocalDate.parse("2024-10-16"), "some desc", 1600),
-            Fine(LocalDate.parse("2024-10-17"), "some desc", 1700),
-            Fine(LocalDate.parse("2024-10-18"), "some desc", 1800),
-            Fine(LocalDate.parse("2024-11-19"), "some desc", 1900),
-            Fine(LocalDate.parse("2024-11-20"), "some desc", 2000),
-            Fine(LocalDate.parse("2024-11-21"), "some desc", 2100),
-            Fine(LocalDate.parse("2024-11-22"), "some desc", 2200),
-            Fine(LocalDate.parse("2024-11-23"), "some desc", 2300),
-            Fine(LocalDate.parse("2024-11-24"), "some desc", 2400),
-            Fine(LocalDate.parse("2024-12-25"), "some desc", 2500),
-            Fine(LocalDate.parse("2024-12-26"), "some desc", 2600),
-            Fine(LocalDate.parse("2024-12-27"), "some desc", 2700),
-            Fine(LocalDate.parse("2024-12-28"), "some desc", 2800),
-            Fine(LocalDate.parse("2024-12-29"), "some desc", 2900),
-            Fine(LocalDate.parse("2024-12-30"), "some desc", 3000)
-        )
     }
 
     private fun setupViewPager(view: View) {
@@ -105,7 +63,7 @@ class MainFragment : Fragment() {
         })
     }
 
-    private fun setupSummaryCard() {
+    private fun setupSummaryCard(view: View) {
         childFragmentManager.commit {
             setReorderingAllowed(true)
             replace(R.id.summaryCard, summaryFragment)
