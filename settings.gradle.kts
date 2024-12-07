bdecad8493b1ba42c858557dcdbffdacbda15aae
evgeniyf <jecky432@gmail.com>
2024-12-08 02:24:04 +0500

    first-structure: settings.gradle.kts (Project Settings): uncomment plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,14 +11,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-//dependencyResolutionManagement {
-//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-//    repositories {
-//        google()
-//        mavenCentral()
-//        gradlePluginPortal()
-//    }
-//}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
 rootProject.name = "project-timely"
 include(":app")
